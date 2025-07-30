5a65b744564f92a8c8045221e389651ac5450860
Valentin <pamerra@mail.ru>
2025-07-30 16:51:00 +0300

    Initial Commit

---
+++ 60.Ch.FriendFace/_0_Ch_FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_Ch_FriendFaceApp.swift
+//  60.Ch.FriendFace
+//
+//  Created by Валентин on 30.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct _0_Ch_FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
